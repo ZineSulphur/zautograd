62ae1325c8fcbdafd2ac2fc5328556d6ebb4d542
ZineSulphur <642735113@qq.com>
2024-06-03 10:13:24 +0800

    添加静态方法标记

--- ad/adlib.py
+++ ad/adlib.py
@@ -3,46 +3,63 @@ import math
 
 def ADlib():
 
-    def ADadd(x, dx, y, dy, z, dz):
+    @staticmethod
+    def ADadd(x, dx, y, dy):
         # x + y -> z
         z = x + y
         dz = dx + dy
-
-    def ADsub(x, dx, y, dy, z, dz):
+        return z, dz
+    
+    @staticmethod
+    def ADsub(x, dx, y, dy):
         # x - y -> z
-        ADadd(x, dx, -y, -dy, z, dz)
+        return ADadd(x, dx, -y, -dy)
 
-    def ADmul(x, dx, y, dy, z, dz):
+    @staticmethod
+    def ADmul(x, dx, y, dy):
         # x * y -> z
         z = x * y
         dz = dx * y + x * dy
+        return z, dz
 
-    def ADdiv(x, dx, y, dy, z, dz):
+    @staticmethod
+    def ADdiv(x, dx, y, dy):
         # x / y -> z
         z = x / y
         dz = dx /y + (x / (y * y)) * dy
+        return z, dz
 
-    def ADpow(x, dx, y, z, dz):
+    @staticmethod
+    def ADpow(x, dx, y):
         # x ** y -> z
         z = x ** y
         dz = y * x ** (y - 1) * dx
+        return z, dz
 
-    def ADexp(x, dx, z, dz):
+    @staticmethod
+    def ADexp(x, dx):
         # z = e ** x
         z = math.exp(x)
         dz = math.exp(x) * dx
+        return z, dz
     
-    def ADlog(x, dx, z, dz):
+    @staticmethod
+    def ADlog(x, dx):
         # z = log(x)
         z = math.log(x)
         dz = 1.0 / x * dx
+        return z, dz
 
-    def ADsin(x, dx, z, dz):
+    @staticmethod
+    def ADsin(x, dx):
         # z = sin(X)
         z = math.sin(x)
         dz = math.cos(x) * dx
+        return z, dz
 
-    def ADcos(x, dx, z, dz):
+    @staticmethod
+    def ADcos(x, dx):
         # z = cos(X)
         z = math.cos(x)
         dz = -1 * math.sin(x) * dx
+        return z, dz
