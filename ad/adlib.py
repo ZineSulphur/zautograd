6e5fd14aab71122c34134cdf2ae218233611330c
ZineSulphur <642735113@qq.com>
2024-06-03 10:00:47 +0800

    添加一些初等函数自动微分方法

--- ad/adlib.py
+++ ad/adlib.py
@@ -1,25 +1,48 @@
 # the Lib base autograd functions
+import math
 
-def ADadd(x, dx, y, dy, z, dz):
-    # x + y -> z
-    z = x + y
-    dz = dx + dy
+def ADlib():
 
-def ADsub(x, dx, y, dy, z, dz):
-    # x - y -> z
-    ADadd(x, dx, -y, -dy, z, dz)
+    def ADadd(x, dx, y, dy, z, dz):
+        # x + y -> z
+        z = x + y
+        dz = dx + dy
 
-def ADmul(x, dx, y, dy, z, dz):
-    # x * y -> z
-    z = x * y
-    dz = dx * y + x * dy
+    def ADsub(x, dx, y, dy, z, dz):
+        # x - y -> z
+        ADadd(x, dx, -y, -dy, z, dz)
 
-def ADdiv(x, dx, y, dy, z, dz):
-    # x / y -> z
-    z = x / y
-    dz = dx /y + (x / (y * y)) * dy
+    def ADmul(x, dx, y, dy, z, dz):
+        # x * y -> z
+        z = x * y
+        dz = dx * y + x * dy
 
-def ADpow(x, dx, y, z, dz):
-    # x ** y -> z
-    z = x ** y
-    dz = y * x ** (y - 1) * dx
+    def ADdiv(x, dx, y, dy, z, dz):
+        # x / y -> z
+        z = x / y
+        dz = dx /y + (x / (y * y)) * dy
+
+    def ADpow(x, dx, y, z, dz):
+        # x ** y -> z
+        z = x ** y
+        dz = y * x ** (y - 1) * dx
+
+    def ADexp(x, dx, z, dz):
+        # z = e ** x
+        z = math.exp(x)
+        dz = math.exp(x) * dx
+    
+    def ADlog(x, dx, z, dz):
+        # z = log(x)
+        z = math.log(x)
+        dz = 1.0 / x * dx
+
+    def ADsin(x, dx, z, dz):
+        # z = sin(X)
+        z = math.sin(x)
+        dz = math.cos(x) * dx
+
+    def ADcos(x, dx, z, dz):
+        # z = cos(X)
+        z = math.cos(x)
+        dz = -1 * math.sin(x) * dx
